16dc976ef22765c1b0c439f689f5ec2fe6f2ad53
CaptainShepard01 <49350081+CaptainShepard01@users.noreply.github.com>
2022-11-24 22:01:33 +0200

    Completed second task, to be proved

--- task_1/main.py
+++ task_1/main.py
@@ -5,55 +5,55 @@ import numpy as np
 
 class BezierCubicSpline:
     def __init__(self):
-        self.mPoints = None
-        self.mA = None
-        self.mB = None
+        self.data_points = None
+        self.first_control_points = None
+        self.second_control_points = None
 
     def fit(self, points):
-        self.mPoints = np.array(points, dtype=np.float32)
+        self.data_points = np.array(points, dtype=np.float32)
 
-        n = len(self.mPoints) - 1
+        n = len(self.data_points) - 1
 
-        C = 4 * np.identity(n)
-        np.fill_diagonal(C[1:], 1)
-        np.fill_diagonal(C[:, 1:], 1)
-        C[0, 0] = 2
-        C[n - 1, n - 1] = 7
-        C[n - 1, n - 2] = 2
+        coefficients_matrix = 4 * np.identity(n)
+        np.fill_diagonal(coefficients_matrix[1:], 1)
+        np.fill_diagonal(coefficients_matrix[:, 1:], 1)
+        coefficients_matrix[0, 0] = 2
+        coefficients_matrix[n - 1, n - 1] = 7
+        coefficients_matrix[n - 1, n - 2] = 2
 
-        P = [2 * (2 * self.mPoints[i] + self.mPoints[i + 1]) for i in range(n)]
-        P[0] = self.mPoints[0] + 2 * self.mPoints[1]
-        P[n - 1] = 8 * self.mPoints[n - 1] + self.mPoints[n]
+        points_vector = [2 * (2 * self.data_points[i] + self.data_points[i + 1]) for i in range(n)]
+        points_vector[0] = self.data_points[0] + 2 * self.data_points[1]
+        points_vector[n - 1] = 8 * self.data_points[n - 1] + self.data_points[n]
 
-        self.mA = np.linalg.solve(C, P)
-        self.mB = [0] * n
+        self.first_control_points = np.linalg.solve(coefficients_matrix, points_vector)
+        self.second_control_points = [0] * n
         for i in range(n - 1):
-            self.mB[i] = 2 * self.mPoints[i + 1] - self.mA[i + 1]
+            self.second_control_points[i] = 2 * self.data_points[i + 1] - self.first_control_points[i + 1]
 
-        self.mB[n - 1] = (self.mA[n - 1] + self.mPoints[n]) / 2
+        self.second_control_points[n - 1] = (self.first_control_points[n - 1] + self.data_points[n]) / 2
 
-    def predict(self, p1, a, b, p2, t):
+    def get_point_on_curve(self, p1, a, b, p2, t):
         return pow(1 - t, 3) * p1 + 3 * pow(1 - t, 2) * t * a + 3 * (1 - t) * t * t * b + t * t * t * p2
 
     def draw(self, img):
-        for i in range(0, len(self.mPoints) - 1):
-            P1 = self.mPoints[i]
-            P2 = self.mPoints[i + 1]
-            A = self.mA[i]
-            B = self.mB[i]
+        for i in range(0, len(self.data_points) - 1):
+            start_point = self.data_points[i]
+            end_point = self.data_points[i + 1]
+            first_control_point = self.first_control_points[i]
+            second_control_point = self.second_control_points[i]
 
             t = np.linspace(0, 1, 50)
             for k in range(0, len(t) - 1):
-                genP1 = self.predict(P1, A, B, P2, t[k])
-                genP2 = self.predict(P1, A, B, P2, t[k + 1])
+                interval_start = self.get_point_on_curve(start_point, first_control_point, second_control_point, end_point, t[k])
+                interval_end = self.get_point_on_curve(start_point, first_control_point, second_control_point, end_point, t[k + 1])
 
-                cv2.line(img, (int(genP1[0]), int(genP1[1])), (int(genP2[0]), int(genP2[1])), (255, 0, 0), 3)
+                cv2.line(img, (int(interval_start[0]), int(interval_start[1])), (int(interval_end[0]), int(interval_end[1])), (255, 0, 0), 3)
 
-            cv2.circle(img, (int(P1[0]), int(P1[1])), 7, (0, 255, 0), -1)
-            cv2.circle(img, (int(P2[0]), int(P2[1])), 7, (0, 255, 0), -1)
+            cv2.circle(img, (int(start_point[0]), int(start_point[1])), 7, (0, 255, 0), -1)
+            cv2.circle(img, (int(end_point[0]), int(end_point[1])), 7, (0, 255, 0), -1)
 
 
-def drawPoints(img, points, color, radius):
+def draw_points(img, points, color, radius):
     for p in points:
         cv2.circle(img, (int(p[0]), int(p[1])), radius, color, -1)
 
@@ -63,13 +63,11 @@ if __name__ == '__main__':
     file = open(filename)
     points = json.load(file)['curve']
 
-    # points.append(points[0])
-
     cubicSpline = BezierCubicSpline()
 
     img = np.ones((1000, 1000, 3), dtype=np.uint8) * 255
 
-    drawPoints(img, points, (255, 0, 0), 5)
+    draw_points(img, points, (255, 0, 0), 5)
     cv2.imshow("Points", img)
     cv2.waitKey(0)
 
--- task_2/main.py
+++ task_2/main.py
@@ -1,6 +1,6 @@
 import json
 import math
-
+import open3d
 import numpy as np
 
 
@@ -12,94 +12,167 @@ def get_distance(first_point, second_point):
 
 class NurbsSurface:
     def __init__(self):
-        self.knot_vectors = None
-        self.basis_functions = None
         self.control_points = None
-        self.grid_points = None
-        self.distances = None
-        self.grid_dimensions = None
-        self.basis_degree = None
+        self.data_points = None
+        self.n = None
+        self.k = None
+        self.s_column = None
+        self.s_row = None
+        self.knots_column = None
+        self.knots_row = None
 
-    # j parameter is to show the desired knot_vector
-    def get_basis_function(self, t, i, k, j):
-        # TODO is it 0 or 1
+    def basis_function(self, t, i, k, knot_vector):
         if k == 0:
-            if self.knot_vectors[j, i] <= t < self.knot_vectors[j, i + 1] or (
-                    self.knot_vectors[j, i] == self.knot_vectors[j, i + 1] and self.knot_vectors[j, i] <= t):
+            # if t == knot_vector[i] or knot_vector[i] < t < knot_vector[i + 1]
+            if knot_vector[i] <= t <= knot_vector[i + 1]:
                 return 1
             return 0
         part_1 = 0.
-        denominator_1 = (self.knot_vectors[j, i + k] - self.knot_vectors[j, i])
+        denominator_1 = (knot_vector[i + k] - knot_vector[i])
         if denominator_1 != 0:
-            part_1 = (t - self.knot_vectors[j, i]) / denominator_1 * self.get_basis_function(t, i, k - 1, j)
+            part_1 = (t - knot_vector[i]) / denominator_1 * self.basis_function(t, i, k - 1, knot_vector)
 
         part_2 = 0.
-        denominator_2 = (self.knot_vectors[j, i + k + 1] - self.knot_vectors[j, i + 1])
+        denominator_2 = (knot_vector[i + k + 1] - knot_vector[i + 1])
         if denominator_2 != 0:
-            part_2 = (self.knot_vectors[j, i + k + 1] - t) / denominator_2 * self.get_basis_function(t, i + 1, k - 1, j)
+            part_2 = (knot_vector[i + k + 1] - t) / denominator_2 * self.basis_function(t, i + 1, k - 1, knot_vector)
 
         return part_1 + part_2
 
-    def initialize_points(self, points, grid, indices):
-        self.grid_dimensions = grid
-        self.grid_points = np.zeros((grid[0], grid[1], 3))
+    def initialize_data_points(self, points, grid, indices, k):
+        self.n = grid[0]
+        self.k = k
+        self.data_points = np.zeros((self.n, self.n, 3))
         for i, grid_position in enumerate(indices):
-            self.grid_points[grid_position[0], grid_position[1]] = points[i]
+            self.data_points[grid_position[0], grid_position[1]] = points[i]
 
-    def initialize_distances(self):
-        n = self.grid_dimensions[0]
-        self.distances = np.zeros((2 * n, n))
+    def find_params_vector(self, points_vector):
+        n = self.n
+        result = np.zeros(n, dtype=np.float32)
+        result[0] = 0.
+        result[n - 1] = 1.
+        d = sum(get_distance(points_vector[i], points_vector[i - 1]) for i in range(1, n))
+        for i in range(1, n - 1):
+            result[i] = result[i - 1] + get_distance(points_vector[i], points_vector[i - 1]) / d
+        return result
+
+    def find_s_vector(self, parameters_matrix):
+        n = self.n
+        result = np.zeros(n, dtype=np.float32)
         for i in range(n):
-            d1 = sum(get_distance(self.grid_points[i, p], self.grid_points[i, p - 1]) for p in
-                     range(1, n))
-            d2 = sum(get_distance(self.grid_points[p, i], self.grid_points[p - 1, i]) for p in
-                     range(1, n))
-            self.distances[i, 0] = 0
-            self.distances[i + n, 0] = 0
-            self.distances[i, n - 1] = 1
-            self.distances[i + n, n - 1] = 1
-            for p in range(1, n - 1):
-                self.distances[i, p] = self.distances[i, p - 1] + get_distance(self.grid_points[i, p],
-                                                                               self.grid_points[i, p - 1]) / d1
-                self.distances[i + n, p] = self.distances[i + n, p - 1] + get_distance(self.grid_points[p, i],
-                                                                                       self.grid_points[p - 1, i]) / d2
+            temp_vector = parameters_matrix[:, i]
+            result[i] = sum(temp_vector[j] for j in range(n)) / n
+        return result
 
-    def initialize_knots(self, k):
-        self.basis_degree = k
-        n = self.grid_dimensions[0]
+    def find_s(self):
+        n = self.n
+        column_matrix = np.zeros((n, n), dtype=np.float32)
+        row_matrix = np.zeros((n, n), dtype=np.float32)
+        for i in range(n):
+            column_matrix[i] = self.find_params_vector(self.data_points[:, i])
+            row_matrix[i] = self.find_params_vector(self.data_points[i])
+
+        self.s_column = self.find_s_vector(column_matrix)
+        self.s_row = self.find_s_vector(row_matrix)
+
+    def generate_knots(self):
+        k = self.k
+        n = self.n
         knots_num = n + k + 1
-        self.knot_vectors = np.zeros((2 * n, knots_num))
-        for i in range(n):
-            for j in range(1, n - k):
-                self.knot_vectors[i, j + k] = 1. / k * sum(self.distances[i, m] for m in range(j, j + k))
-                self.knot_vectors[i + n, j + k] = 1. / k * sum(self.distances[i + n, m] for m in range(j, j + k))
-            for j in range(k + 1):
-                self.knot_vectors[i, j] = 0.
-                self.knot_vectors[i + n, j] = 0.
-                self.knot_vectors[i, knots_num - j - 1] = 1.
-                self.knot_vectors[i + n, knots_num - j - 1] = 1.
+        self.knots_column = np.zeros(knots_num, dtype=np.float32)
+        self.knots_row = np.zeros(knots_num, dtype=np.float32)
+        for j in range(1, n - k):
+            self.knots_column[j + k] = 1. / k * sum(self.s_column[m] for m in range(j, j + k))
+            self.knots_row[j + k] = 1. / k * sum(self.s_row[m] for m in range(j, j + k))
+        for j in range(k + 1):
+            self.knots_column[j] = 0.
+            self.knots_row[j] = 0.
+            self.knots_column[knots_num - j - 1] = 1.
+            self.knots_row[knots_num - j - 1] = 1.
 
-    def find_control_points(self):
-        n = self.grid_dimensions[0]
-        self.control_points = np.zeros((2 * n, n, 3))
+    def generate_control_points(self):
+        n = self.n
+        k = self.k
+        q_matrix = np.zeros((n, n, 3))
+        self.control_points = np.zeros((n, n, 3))
+        temp_matrix = np.zeros((n, n), dtype=np.float32)
+        # temp_matrix[0, 0] = 1.
+        # temp_matrix[n - 1, n - 1] = 1.
 
-        for row in range(n):
-            n_matrix1 = np.zeros((n, n), dtype=np.float32)
-            n_matrix2 = np.zeros((n, n), dtype=np.float32)
-            n_matrix1[0, 0] = 1.
-            n_matrix2[0, 0] = 1.
-            n_matrix1[n - 1, n - 1] = 1.
-            n_matrix2[n - 1, n - 1] = 1.
-            for point_index in range(1, n - 1):
+        for d in range(n):
+            for i in range(n):
+                # for j in range(1, n-1):
                 for j in range(n):
-                    n_matrix1[point_index, j] = self.get_basis_function(self.distances[row, point_index], j,
-                                                                        self.basis_degree, row)
-                    n_matrix2[point_index, j] = self.get_basis_function(self.distances[row + n, point_index], j,
-                                                                        self.basis_degree, row + n)
+                    temp_matrix[i, j] = self.basis_function(self.s_column[i], j, k, self.knots_column)
+            q_matrix[:, d] = np.linalg.solve(temp_matrix, self.data_points[:, d])
 
-            self.control_points[row] = np.linalg.solve(n_matrix1, self.grid_points[row])
-            self.control_points[row + n] = np.linalg.solve(n_matrix2, self.grid_points[:, row])
+        for c in range(n):
+            for i in range(1, n - 1):
+                for j in range(n):
+                    temp_matrix[i, j] = self.basis_function(self.s_row[i], j, k, self.knots_row)
+            self.control_points[c] = np.linalg.solve(temp_matrix, q_matrix[c])
 
+    def get_point_on_surface(self, u, v):
+        k = self.k
+        n = self.n
+        result = np.zeros(3)
+        for i in range(n):
+            for j in range(n):
+                n_i_k = self.basis_function(u, i, k, self.knots_column)
+                n_j_k = self.basis_function(v, j, k, self.knots_row)
+                result += n_i_k * n_j_k * self.control_points[i, j]
+
+        return result
+
+    def get_surface_mesh(self, points_count):
+        surface_points = []
+        u = np.linspace(0, 1, points_count)
+        v = np.linspace(0, 1, points_count)
+        for i in range(len(u)):
+            for j in range(len(v)):
+                surface_points.append(self.get_point_on_surface(u[i], v[j]))
+        return surface_points
+
+
+def matrix2array(matrix):
+    n = len(matrix)
+    m = len(matrix[0])
+    result_array = np.zeros((n * n, 3))
+    for i in range(n):
+        for j in range(m):
+            result_array[i * n + j] = matrix[i, j]
+    return result_array
+
+
+def get_triangles(n):
+    result_array = []
+    for i in range(n - 1):
+        for j in range(n - 1):
+            result_array.append(np.array([i * n + j, i * n + j + 1, i * n + j + n]).astype(np.int32))
+            result_array.append(np.array([i * n + j + 1, i * n + j + n, i * n + j + n + 1]).astype(np.int32))
+
+    return np.asarray(result_array)
+
+
+def get_mesh(surface):
+    point_count = 10
+    points_array = surface.get_surface_mesh(point_count)
+    mesh = open3d.geometry.TriangleMesh()
+    mesh.vertices = open3d.utility.Vector3dVector(points_array)
+    triangles = get_triangles(point_count)
+    mesh.triangles = open3d.utility.Vector3iVector(triangles)
+    return mesh
+
+
+def visualize(surface):
+    input_points = open3d.geometry.PointCloud()
+    input_points.points = open3d.utility.Vector3dVector(matrix2array(surface.data_points))
+
+    control_points = open3d.geometry.PointCloud()
+    control_points.points = open3d.utility.Vector3dVector(matrix2array(surface.control_points))
+
+    open3d.visualization.draw_geometries([input_points, get_mesh(surface)], mesh_show_back_face=True,
+                                         mesh_show_wireframe=True)
 
 if __name__ == '__main__':
     filename = "E:/Projects/University/MBCG_Module_2/task_2/resources/1.json"
@@ -110,8 +183,9 @@ if __name__ == '__main__':
     grid = data["gridSize"]
 
     surface = NurbsSurface()
-    surface.initialize_points(points, grid, indices)
-    surface.initialize_distances()
-    surface.initialize_knots(3)
-    surface.find_control_points()
-    print("Stop")
+    surface.initialize_data_points(points, grid, indices, 3)
+    surface.find_s()
+    surface.generate_knots()
+    surface.generate_control_points()
+
+    visualize(surface)
